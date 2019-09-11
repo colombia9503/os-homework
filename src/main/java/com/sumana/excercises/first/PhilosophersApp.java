f2909b7ee00ba6f7fb3036aeba508afab2e7a782
sumana <sumana@devmachine.tk>
2019-09-11 02:15:03 -0500

    hungry philosophers algorithm solution

--- src/main/java/com/sumana/excercises/first/PhilosophersApp.java
+++ src/main/java/com/sumana/excercises/first/PhilosophersApp.java
@@ -1,4 +1,86 @@
 package com.sumana.excercises.first;
 
+import com.sumana.excercises.utils.ThreadColor;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class PhilosophersApp {
+    public static void main(String[] args) {
+        //Reference for the 5 forks on the table, each philosopher can access to 2 different forks at the time
+        Fork fork1 = new Fork();
+        Fork fork2 = new Fork();
+        Fork fork3 = new Fork();
+        Fork fork4 = new Fork();
+        Fork fork5 = new Fork();
+
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        //hp can access to for
+        HungryPhilosopher hungryPhilosopher1 = new HungryPhilosopher(fork1, fork5, ThreadColor.ANSI_CYAN);
+        HungryPhilosopher hungryPhilosopher2 = new HungryPhilosopher(fork2, fork1, ThreadColor.ANSI_PURPLE);
+        HungryPhilosopher hungryPhilosopher3 = new HungryPhilosopher(fork3, fork2, ThreadColor.ANSI_YELLOW);
+        HungryPhilosopher hungryPhilosopher4 = new HungryPhilosopher(fork4, fork3, ThreadColor.ANSI_GREEN);
+        HungryPhilosopher hungryPhilosopher5 = new HungryPhilosopher(fork5, fork4, ThreadColor.ANSI_RED);
+
+        new Thread(hungryPhilosopher1).start();
+        new Thread(hungryPhilosopher2).start();
+        new Thread(hungryPhilosopher3).start();
+        new Thread(hungryPhilosopher4).start();
+        new Thread(hungryPhilosopher5).start();
+
+        /*executorService.execute(hungryPhilosopher1);
+        executorService.execute(hungryPhilosopher2);
+        executorService.execute(hungryPhilosopher3);
+        executorService.execute(hungryPhilosopher4);
+        executorService.execute(hungryPhilosopher5);*/
+    }
+}
+
+class Fork {
+    private ReentrantLock lock = new ReentrantLock();
+
+    public void useFork(int eatingPastaTime) {
+        lock.lock();
+        try {
+            Thread.sleep(eatingPastaTime);
+        } catch (InterruptedException e) {
+            System.err.println("Fork got interrupted by another philosopher!");
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean onUse() {
+        return lock.isLocked();
+    }
+}
+
+class HungryPhilosopher implements Runnable {
+    private boolean hungry = false;
+    private Fork leftFork;
+    private Fork rightFork;
+    private String color;
+    private final Random random = new Random();
+
+    public HungryPhilosopher(Fork leftFork, Fork rightFork, String color) {
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+        this.color = color;
+    }
+
+    @Override
+    public void run() {
+        while (hungry) {
+            if (!rightFork.onUse() && !leftFork.onUse()) {
+                Integer eatingPastaTime = random.nextInt(500);
+                rightFork.useFork(eatingPastaTime);
+                leftFork.useFork(eatingPastaTime);
+                hungry = false;
+            }
+        }
+
+        System.out.println(color + "I have already eat");
+    }
 }
--- src/main/java/com/sumana/excercises/third/ProducerConsumerApp.java
+++ src/main/java/com/sumana/excercises/third/ProducerConsumerApp.java
@@ -1,97 +1,116 @@
 package com.sumana.excercises.third;
 
+import com.sumana.excercises.utils.ThreadColor;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static com.sumana.excercises.third.ProducerConsumerApp.EOF;
 
 public class ProducerConsumerApp {
+    public static final String EOF = "EOF";
 
     public static void main(String[] args) {
-        Message message = new Message();
-        (new Thread(new Writer(message))).start();
-        (new Thread(new Reader(message))).start();
-    }
+        List<String> buffer = new ArrayList<>();
+        ReentrantLock bufferLock = new ReentrantLock();
 
-}
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-class Message {
-    private String message;
-    private boolean empty = true;
+        MyProducer myProducer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW, bufferLock);
+        MyConsumer myConsumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
+        MyConsumer myConsumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock);
 
-    public synchronized String read() {
-        while (empty) {
-            try {
-               wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        empty = true;
-        notifyAll();
-        return message;
-    }
-
-    public synchronized void write(String message) {
-        while (!empty) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        empty = false;
-        this.message = message;
-        notifyAll();
+        executorService.execute(myProducer);
+        executorService.execute(myConsumer1);
+        executorService.execute(myConsumer2);
     }
 }
 
-class Writer implements Runnable {
-    private Message message;
+class MyProducer implements Runnable {
+    private List<String> buffer;
+    private String color;
+    private ReentrantLock bufferLock;
 
-    public Writer(Message message) {
-        this.message = message;
+    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
+        this.buffer = buffer;
+        this.color = color;
+        this.bufferLock = bufferLock;
     }
 
-    @Override
-    public void run() {
-        String messages[] = {
-                "Hello this is the first line",
-                "But there is it, a second line!!",
-                "but not enough, there is a third line!!!!",
-                "HE HE"
-        };
-
-        Random random = new Random();
-
-        for (String message : messages) {
-            this.message.write(message);
-            try {
-                Thread.sleep(random.nextInt(2000));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        this.message.write("EOF");
-    }
-}
-
-class Reader implements Runnable {
-
-    public Message message;
-
-    public Reader(Message message) {
-        this.message = message;
+    public MyProducer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
     }
 
     @Override
     public void run() {
         Random random = new Random();
+        String[] nums = {"1", "2", "3", "4", "5"};
 
-        for (String latestMessage = message.read(); !latestMessage.equals("EOF"); latestMessage = message.read()) {
-            System.out.println(latestMessage);
+        for (String num : nums) {
             try {
-                Thread.sleep(random.nextInt(2000));
+                System.out.println(color + "Adding... " + num);
+                bufferLock.lock();
+                try {
+                    buffer.add(num);
+                } finally {
+                    bufferLock.unlock();
+                }
+                Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                System.err.println("The producer was interrupted!");
             }
+        }
+
+        System.out.println(color + "Adding EOF and exiting...");
+        bufferLock.lock();
+        try {
+            buffer.add(EOF);
+        } finally {
+            bufferLock.unlock();
         }
     }
 }
+
+class MyConsumer implements Runnable {
+    private List<String> buffer;
+    private String color;
+    private ReentrantLock bufferLock;
+
+    public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
+        this.buffer = buffer;
+        this.color = color;
+        this.bufferLock = bufferLock;
+    }
+
+    public MyConsumer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+                bufferLock.lock();
+                try {
+                    if (buffer.isEmpty()) {
+                        continue;
+                    }
+
+                    if (buffer.get(0).equals(EOF)) {
+                        System.out.println(color + "Exiting");
+                        break;
+                    } else {
+                        System.out.println(color + "Removed: " + buffer.remove(0));
+                    }
+                } finally {
+                    bufferLock.unlock();
+                }
+        }
+    }
+}
+
